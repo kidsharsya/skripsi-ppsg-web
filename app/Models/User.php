2f6d6d205d75b57643a81aaa36e2c00d91260716
Krisna Harsya Saputra <129250588+kidsharsya@users.noreply.github.com>
2025-05-23 21:33:13 +0700

    Update can access panel user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Filament\Panel;
-use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role == 'admin';
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -55,10 +61,6 @@ class User extends Authenticatable
     // return true;
     // }
 
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, 'superadmin1@gmail.com') && $this->hasVerifiedEmail();
-    }
 
 
     // Fungsi untuk memeriksa apakah pengguna adalah admin
